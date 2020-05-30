6a1ba17c77be31ea63d0da3233b0e4c5d6af8597
Freddy Esneyder Jaimes Ruiz <fred.jaimes@gmail.com>
2020-05-30 15:05:52 -0500

    [Add] Login Screen

---
+++ app/src/main/java/com/reciapp/gatherer/ui/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.reciapp.gatherer.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.reciapp.gatherer.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
